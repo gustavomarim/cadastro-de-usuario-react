28dffb901abb59f313880418d6a2b4123d216e4f
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-07-04 13:52:32 -0300

    Update - Footer component

--- crud/frontend/src/components/template/Footer.jsx
+++ crud/frontend/src/components/template/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const fn = props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Gust<span className="text-danger">4</span>vo</strong>
+        </span>
     </footer>
 
 export default fn
