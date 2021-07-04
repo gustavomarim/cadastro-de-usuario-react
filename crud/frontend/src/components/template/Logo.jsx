e04b9ebbd47ab0af10258dc9252e86cb24ecfa1a
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-07-04 13:35:52 -0300

    Update - Logo component

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -1,9 +1,12 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
-const fn = props => 
-<aside className="logo">
-    Logo
-</aside>
+const fn = props =>
+    <aside className="logo">
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
+    </aside>
 
 export default fn
