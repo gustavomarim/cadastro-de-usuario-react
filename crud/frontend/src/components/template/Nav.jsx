c379210f2f686d439f180c7bc21658fdf5d1c719
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-07-04 14:14:41 -0300

    Update - Nav component

--- crud/frontend/src/components/template/Nav.jsx
+++ crud/frontend/src/components/template/Nav.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 
 const fn = props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+
+            {/* Refatorar em casa! */}
+            <a href="/">
+                <i className="fa fa-home"></i> Início
+            </a>
+
+            <a href to="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+            
+        </nav>
     </aside>
 
 export default fn
