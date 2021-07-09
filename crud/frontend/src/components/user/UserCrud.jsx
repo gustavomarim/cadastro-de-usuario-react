4659c5ea740233cd85991760d57537a31f527d18
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-07-09 11:22:18 -0300

    Update - User registration component #04

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -2,36 +2,36 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Main from '../template/Main'
 
-// Componente criado em classe por possuir estado
 const headerProps = {
     icon: 'users',
     title: 'Usuários',
-    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar, Excluir'
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
-// DESAFIO -  Estado inicial (Colocar dentro de constante -> utilizar import/export)
 const baseUrl = 'http://localhost:3001/users'
 const initialState = {
     user: { name: '', email: '' },
     list: []
 }
 
-// DESAFIO - Componentizar o Incluir, Listar... (row 8)
 export default class UserCrud extends Component {
 
     state = { ...initialState }
 
-    // Limpa o form.
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
+        })
+    }
+
     clear() {
         this.setState({ user: initialState.user })
     }
 
-    // Inserir/Alterar user existente
     save() {
         const user = this.state.user
         const method = user.id ? 'put' : 'post'
         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
-
         axios[method](url, user)
             .then(resp => {
                 const list = this.getUpdatedList(resp.data)
@@ -39,14 +39,13 @@ export default class UserCrud extends Component {
             })
     }
 
-    getUpdatedList(user) {
+    getUpdatedList(user, add = true) {
         const list = this.state.list.filter(u => u.id !== user.id)
-        list.unshift(user)
+        if(add) list.unshift(user)
         return list
     }
 
     updateField(event) {
-        // Sempre que um dado for alterado, a boa prática é clonar e dps setar
         const user = { ...this.state.user }
         user[event.target.name] = event.target.value
         this.setState({ user })
@@ -54,18 +53,16 @@ export default class UserCrud extends Component {
 
     renderForm() {
         return (
-
             <div className="form">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <div className="form-group">
-                            <label >Nome</label>
+                            <label>Nome</label>
                             <input type="text" className="form-control"
                                 name="name"
-                                value={this.state.user.value}
+                                value={this.state.user.name}
                                 onChange={e => this.updateField(e)}
-                                placeholder="Digite o nome..."
-                            />
+                                placeholder="Digite o nome..." />
                         </div>
                     </div>
 
@@ -76,36 +73,85 @@ export default class UserCrud extends Component {
                                 name="email"
                                 value={this.state.user.email}
                                 onChange={e => this.updateField(e)}
-                                placeholder="Digite o E-mail..."
-                            />
-                        </div>{/* form-group */}
-                    </div>{/* "col-12 col-md-6" */}
-
-                </div> {/* row */}
+                                placeholder="Digite o e-mail..." />
+                        </div>
+                    </div>
+                </div>
 
                 <hr />
-
                 <div className="row">
-                    <div className="col-12 d-flex">
-                        <button className="bnt btn-primary"
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary mt-0 m-1"
                             onClick={e => this.save(e)}>
                             Salvar
                         </button>
-                        <button className="btn btn-secondary ml-2"
+
+                        <button className="btn btn-secondary mt-0 m-1"
                             onClick={e => this.clear(e)}>
                             Cancelar
                         </button>
-                    </div>{/* col-12 d-flex justify-content-end */}
-                </div>{/* row */}
-
+                    </div>
+                </div>
             </div>
         )
     }
 
+    load(user) {
+        this.setState({ user })
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false)
+            this.setState({ list })
+        })
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows() {
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning mt-0 m-1"
+                            onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger mt-0 m-1"
+                            onClick={() => this.remove(user)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+    
     render() {
         return (
-            <Main {...headerProps} >
+            <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
