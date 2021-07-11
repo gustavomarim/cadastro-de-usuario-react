8eb99e8a61679667916a914429fe4334ddd68d03
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-07-11 18:03:16 -0300

    Update

--- crud/frontend/src/components/template/Footer.jsx
+++ crud/frontend/src/components/template/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const fn = props =>
     <footer className="footer">
         <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Gust<span className="text-danger">4</span>vo</strong>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Gustavo Dantas</strong>
         </span>
     </footer>
 
